f4845f9e9c9a3b2d3d288cca01b5b2c6d08b0fc8
pelpel <christopherabuloc@gmail.com>
2020-03-14 01:38:59 +0800

    First commit

---
+++ Calculatorv2/Form1.Designer.vb
@@ -0,0 +1,393 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Text_sc = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Btn_numpercent = New System.Windows.Forms.Button()
+        Me.Btn_numdivide = New System.Windows.Forms.Button()
+        Me.Btn_num9 = New System.Windows.Forms.Button()
+        Me.Btn_num8 = New System.Windows.Forms.Button()
+        Me.Btn_num7 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Btn_numtimes = New System.Windows.Forms.Button()
+        Me.Btn_num6 = New System.Windows.Forms.Button()
+        Me.Btn_num5 = New System.Windows.Forms.Button()
+        Me.Btn_num4 = New System.Windows.Forms.Button()
+        Me.Btn_numminus = New System.Windows.Forms.Button()
+        Me.Btn_num3 = New System.Windows.Forms.Button()
+        Me.Btn_num2 = New System.Windows.Forms.Button()
+        Me.Btn_num1 = New System.Windows.Forms.Button()
+        Me.Button26 = New System.Windows.Forms.Button()
+        Me.Btn_numplus = New System.Windows.Forms.Button()
+        Me.Button28 = New System.Windows.Forms.Button()
+        Me.Btn_num0 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Text_sc
+        '
+        Me.Text_sc.Location = New System.Drawing.Point(16, 16)
+        Me.Text_sc.Margin = New System.Windows.Forms.Padding(4)
+        Me.Text_sc.MaximumSize = New System.Drawing.Size(4, 40)
+        Me.Text_sc.MinimumSize = New System.Drawing.Size(399, 40)
+        Me.Text_sc.Name = "Text_sc"
+        Me.Text_sc.Size = New System.Drawing.Size(399, 40)
+        Me.Text_sc.TabIndex = 0
+        Me.Text_sc.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(16, 76)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 38)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "MC"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(97, 76)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 38)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "MR"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(178, 76)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 38)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "MS"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(259, 76)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 38)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "M+"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(340, 76)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 38)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "M-"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(340, 120)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 38)
+        Me.Button6.TabIndex = 9
+        Me.Button6.Text = "√"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(259, 120)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(75, 38)
+        Me.Button7.TabIndex = 8
+        Me.Button7.Text = "±"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(178, 120)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(75, 38)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "C"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(97, 120)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(75, 38)
+        Me.Button9.TabIndex = 6
+        Me.Button9.Text = "CE"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(16, 120)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(75, 38)
+        Me.Button10.TabIndex = 5
+        Me.Button10.Text = "←"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Btn_numpercent
+        '
+        Me.Btn_numpercent.Location = New System.Drawing.Point(340, 164)
+        Me.Btn_numpercent.Name = "Btn_numpercent"
+        Me.Btn_numpercent.Size = New System.Drawing.Size(75, 38)
+        Me.Btn_numpercent.TabIndex = 14
+        Me.Btn_numpercent.Text = "%"
+        Me.Btn_numpercent.UseVisualStyleBackColor = True
+        '
+        'Btn_numdivide
+        '
+        Me.Btn_numdivide.Location = New System.Drawing.Point(259, 164)
+        Me.Btn_numdivide.Name = "Btn_numdivide"
+        Me.Btn_numdivide.Size = New System.Drawing.Size(75, 38)
+        Me.Btn_numdivide.TabIndex = 13
+        Me.Btn_numdivide.Text = "/"
+        Me.Btn_numdivide.UseVisualStyleBackColor = True
+        '
+        'Btn_num9
+        '
+        Me.Btn_num9.Location = New System.Drawing.Point(178, 164)
+        Me.Btn_num9.Name = "Btn_num9"
+        Me.Btn_num9.Size = New System.Drawing.Size(75, 38)
+        Me.Btn_num9.TabIndex = 12
+        Me.Btn_num9.Text = "9"
+        Me.Btn_num9.UseVisualStyleBackColor = True
+        '
+        'Btn_num8
+        '
+        Me.Btn_num8.Location = New System.Drawing.Point(97, 164)
+        Me.Btn_num8.Name = "Btn_num8"
+        Me.Btn_num8.Size = New System.Drawing.Size(75, 38)
+        Me.Btn_num8.TabIndex = 11
+        Me.Btn_num8.Text = "8"
+        Me.Btn_num8.UseVisualStyleBackColor = True
+        '
+        'Btn_num7
+        '
+        Me.Btn_num7.Location = New System.Drawing.Point(16, 164)
+        Me.Btn_num7.Name = "Btn_num7"
+        Me.Btn_num7.Size = New System.Drawing.Size(75, 38)
+        Me.Btn_num7.TabIndex = 10
+        Me.Btn_num7.Text = "7"
+        Me.Btn_num7.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(340, 208)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(75, 38)
+        Me.Button16.TabIndex = 19
+        Me.Button16.Text = "1/x"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Btn_numtimes
+        '
+        Me.Btn_numtimes.Location = New System.Drawing.Point(259, 208)
+        Me.Btn_numtimes.Name = "Btn_numtimes"
+        Me.Btn_numtimes.Size = New System.Drawing.Size(75, 38)
+        Me.Btn_numtimes.TabIndex = 18
+        Me.Btn_numtimes.Text = "*"
+        Me.Btn_numtimes.UseVisualStyleBackColor = True
+        '
+        'Btn_num6
+        '
+        Me.Btn_num6.Location = New System.Drawing.Point(178, 208)
+        Me.Btn_num6.Name = "Btn_num6"
+        Me.Btn_num6.Size = New System.Drawing.Size(75, 38)
+        Me.Btn_num6.TabIndex = 17
+        Me.Btn_num6.Text = "6"
+        Me.Btn_num6.UseVisualStyleBackColor = True
+        '
+        'Btn_num5
+        '
+        Me.Btn_num5.Location = New System.Drawing.Point(97, 208)
+        Me.Btn_num5.Name = "Btn_num5"
+        Me.Btn_num5.Size = New System.Drawing.Size(75, 38)
+        Me.Btn_num5.TabIndex = 16
+        Me.Btn_num5.Text = "5"
+        Me.Btn_num5.UseVisualStyleBackColor = True
+        '
+        'Btn_num4
+        '
+        Me.Btn_num4.Location = New System.Drawing.Point(16, 208)
+        Me.Btn_num4.Name = "Btn_num4"
+        Me.Btn_num4.Size = New System.Drawing.Size(75, 38)
+        Me.Btn_num4.TabIndex = 15
+        Me.Btn_num4.Text = "4"
+        Me.Btn_num4.UseVisualStyleBackColor = True
+        '
+        'Btn_numminus
+        '
+        Me.Btn_numminus.Location = New System.Drawing.Point(259, 252)
+        Me.Btn_numminus.Name = "Btn_numminus"
+        Me.Btn_numminus.Size = New System.Drawing.Size(75, 38)
+        Me.Btn_numminus.TabIndex = 23
+        Me.Btn_numminus.Text = "-"
+        Me.Btn_numminus.UseVisualStyleBackColor = True
+        '
+        'Btn_num3
+        '
+        Me.Btn_num3.Location = New System.Drawing.Point(178, 252)
+        Me.Btn_num3.Name = "Btn_num3"
+        Me.Btn_num3.Size = New System.Drawing.Size(75, 38)
+        Me.Btn_num3.TabIndex = 22
+        Me.Btn_num3.Text = "3"
+        Me.Btn_num3.UseVisualStyleBackColor = True
+        '
+        'Btn_num2
+        '
+        Me.Btn_num2.Location = New System.Drawing.Point(97, 252)
+        Me.Btn_num2.Name = "Btn_num2"
+        Me.Btn_num2.Size = New System.Drawing.Size(75, 38)
+        Me.Btn_num2.TabIndex = 21
+        Me.Btn_num2.Text = "2"
+        Me.Btn_num2.UseVisualStyleBackColor = True
+        '
+        'Btn_num1
+        '
+        Me.Btn_num1.Location = New System.Drawing.Point(16, 252)
+        Me.Btn_num1.Name = "Btn_num1"
+        Me.Btn_num1.Size = New System.Drawing.Size(75, 38)
+        Me.Btn_num1.TabIndex = 20
+        Me.Btn_num1.Text = "1"
+        Me.Btn_num1.UseVisualStyleBackColor = True
+        '
+        'Button26
+        '
+        Me.Button26.Location = New System.Drawing.Point(340, 252)
+        Me.Button26.Name = "Button26"
+        Me.Button26.Size = New System.Drawing.Size(75, 82)
+        Me.Button26.TabIndex = 29
+        Me.Button26.Text = "="
+        Me.Button26.UseVisualStyleBackColor = True
+        '
+        'Btn_numplus
+        '
+        Me.Btn_numplus.Location = New System.Drawing.Point(259, 296)
+        Me.Btn_numplus.Name = "Btn_numplus"
+        Me.Btn_numplus.Size = New System.Drawing.Size(75, 38)
+        Me.Btn_numplus.TabIndex = 28
+        Me.Btn_numplus.Text = "+"
+        Me.Btn_numplus.UseVisualStyleBackColor = True
+        '
+        'Button28
+        '
+        Me.Button28.Location = New System.Drawing.Point(178, 296)
+        Me.Button28.Name = "Button28"
+        Me.Button28.Size = New System.Drawing.Size(75, 38)
+        Me.Button28.TabIndex = 27
+        Me.Button28.Text = "."
+        Me.Button28.UseVisualStyleBackColor = True
+        '
+        'Btn_num0
+        '
+        Me.Btn_num0.Location = New System.Drawing.Point(16, 296)
+        Me.Btn_num0.Name = "Btn_num0"
+        Me.Btn_num0.Size = New System.Drawing.Size(156, 38)
+        Me.Btn_num0.TabIndex = 25
+        Me.Btn_num0.Text = "0"
+        Me.Btn_num0.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(433, 354)
+        Me.Controls.Add(Me.Button26)
+        Me.Controls.Add(Me.Btn_numplus)
+        Me.Controls.Add(Me.Button28)
+        Me.Controls.Add(Me.Btn_num0)
+        Me.Controls.Add(Me.Btn_numminus)
+        Me.Controls.Add(Me.Btn_num3)
+        Me.Controls.Add(Me.Btn_num2)
+        Me.Controls.Add(Me.Btn_num1)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Btn_numtimes)
+        Me.Controls.Add(Me.Btn_num6)
+        Me.Controls.Add(Me.Btn_num5)
+        Me.Controls.Add(Me.Btn_num4)
+        Me.Controls.Add(Me.Btn_numpercent)
+        Me.Controls.Add(Me.Btn_numdivide)
+        Me.Controls.Add(Me.Btn_num9)
+        Me.Controls.Add(Me.Btn_num8)
+        Me.Controls.Add(Me.Btn_num7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Text_sc)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Text_sc As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Btn_numpercent As Button
+    Friend WithEvents Btn_numdivide As Button
+    Friend WithEvents Btn_num9 As Button
+    Friend WithEvents Btn_num8 As Button
+    Friend WithEvents Btn_num7 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Btn_numtimes As Button
+    Friend WithEvents Btn_num6 As Button
+    Friend WithEvents Btn_num5 As Button
+    Friend WithEvents Btn_num4 As Button
+    Friend WithEvents Btn_numminus As Button
+    Friend WithEvents Btn_num3 As Button
+    Friend WithEvents Btn_num2 As Button
+    Friend WithEvents Btn_num1 As Button
+    Friend WithEvents Button26 As Button
+    Friend WithEvents Btn_numplus As Button
+    Friend WithEvents Button28 As Button
+    Friend WithEvents Btn_num0 As Button
+
+
+End Class
---
+++ Calculatorv2/Form1.vb
@@ -0,0 +1,96 @@
+﻿Public Class Form1
+    Private Sub Btn_num0_Click(sender As Object, e As EventArgs) Handles Btn_num0.Click
+        If Text_sc.Text = "0" Then
+            Text_sc.Text = "0"
+
+        Else
+            Text_sc.Text = Text_sc.Text + "0"
+        End If
+    End Sub
+
+    Private Sub Btn_num1_Click(sender As Object, e As EventArgs) Handles Btn_num1.Click
+        If Text_sc.Text = "0" Then
+            Text_sc.Text = "1"
+
+        Else
+            Text_sc.Text = Text_sc.Text + "1"
+        End If
+    End Sub
+
+    Private Sub Btn_num3_Click(sender As Object, e As EventArgs) Handles Btn_num3.Click
+        If Text_sc.Text = "0" Then
+            Text_sc.Text = "3"
+
+        Else
+            Text_sc.Text = Text_sc.Text + "3"
+        End If
+    End Sub
+
+    Private Sub Btn_num2_Click(sender As Object, e As EventArgs) Handles Btn_num2.Click
+        If Text_sc.Text = "0" Then
+            Text_sc.Text = "2"
+
+        Else
+            Text_sc.Text = Text_sc.Text + "2"
+        End If
+    End Sub
+
+    Private Sub Btn_num4_Click(sender As Object, e As EventArgs) Handles Btn_num4.Click
+        If Text_sc.Text = "0" Then
+            Text_sc.Text = "4"
+
+        Else
+            Text_sc.Text = Text_sc.Text + "4"
+        End If
+    End Sub
+
+    Private Sub Btn_num5_Click(sender As Object, e As EventArgs) Handles Btn_num5.Click
+        If Text_sc.Text = "0" Then
+            Text_sc.Text = "5"
+
+        Else
+            Text_sc.Text = Text_sc.Text + "5"
+        End If
+    End Sub
+
+    Private Sub Btn_num6_Click(sender As Object, e As EventArgs) Handles Btn_num6.Click
+        If Text_sc.Text = "0" Then
+            Text_sc.Text = "6"
+
+        Else
+            Text_sc.Text = Text_sc.Text + "6"
+        End If
+    End Sub
+
+    Private Sub Btn_num7_Click(sender As Object, e As EventArgs) Handles Btn_num7.Click
+        If Text_sc.Text = "0" Then
+            Text_sc.Text = "7"
+
+        Else
+            Text_sc.Text = Text_sc.Text + "7"
+        End If
+    End Sub
+    Private Sub Btn_num8_Click(sender As Object, e As EventArgs) Handles Btn_num8.Click
+        If Text_sc.Text = "0" Then
+            Text_sc.Text = "8"
+
+        Else
+            Text_sc.Text = Text_sc.Text + "8"
+        End If
+    End Sub
+
+    Private Sub Btn_num9_Click(sender As Object, e As EventArgs) Handles Btn_num9.Click
+        If Text_sc.Text = "0" Then
+            Text_sc.Text = "9"
+
+        Else
+            Text_sc.Text = Text_sc.Text + "9"
+        End If
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        If Text_sc.Text.Length > 0 Then
+            Text_sc.Text = "0"
+        End If
+    End Sub
+End Class
